64c72cf2172226b9b7ddb5733f0c04434ea903cb
Charles Deotto <charles.deotto@gmail.com>
2023-09-26 15:50:57 +0200

    Listes et debut tutos

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,49 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show edit update destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :task_names,)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :reservations
-
+  resources :tutos
+  resources :lists do
+    resources :tasks
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
