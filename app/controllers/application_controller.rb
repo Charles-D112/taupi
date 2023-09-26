1d9650955ffee5e9343e758536b7671c85d7a96d
Charles Deotto <charles.deotto@gmail.com>
2023-09-26 17:50:34 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from ActiveRecord::RecordNotFound, with: :not_found
-    rescue_from StandardError, with: :internal_server_error
-  end
-
-  def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
-  end
 end
